0665f00158581385f559a9d007f7e876415c4741
nagver <135354625+nagver@users.noreply.github.com>
2025-09-11 11:24:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Python utility to interface with esp32 Blufi component.",
     packages=find_packages(),
     install_requires=[
-        'cryptography==45.0.7',
-        'bleak==1.1.1'
+        'cryptography>=45.0.7',
+        'bleak>=1.1.1'
     ],
 )
