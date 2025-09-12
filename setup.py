99153739ee89e4a7a0a18f1ad302e7b7cfcaf22d
Nagendra Verma <nagendra.verma-ext@ab-inbev.com>
2025-09-12 22:46:21 +0100

    - Updated to work with crypto 45.0.7

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyBlufi',
-    version='0.0.1',
+    version='0.0.3',
     description="Python utility to interface with esp32 Blufi component.",
     packages=find_packages(),
     install_requires=[
-        'cryptography>=38.0.4',
+        'cryptography>=45.0.7',
         'bleak>=1.1.1'
     ],
 )
