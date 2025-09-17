5f4e707ab0446c4efacecb110254328ec4c68805
Nagendra Verma <nagendra.verma-ext@ab-inbev.com>
2025-09-17 10:43:00 +0100

    - Updated version to v0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyBlufi',
-    version='0.0.3',
+    version='0.0.6',
     description="Python utility to interface with esp32 Blufi component.",
     packages=find_packages(),
     install_requires=[
